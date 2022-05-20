2169917f7ec09955010f4950aed0937a9d13ac17
Emmanuel-mg <75080253+Emmanuel-mg@users.noreply.github.com>
2022-05-20 11:07:35 +0200

    Delete Preprocessing directory

--- Preprocessing/README.md
+++
@@ -1,1 +0,0 @@
-<h1>Preprocessing des images</h1>
--- Preprocessing/resizing.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import glob
-import os
-
-for i in glob.glob('*.jpg'):
-    img = cv2.imread(i)
-    resized_img = cv2.resize(img, (256,144))
-    cv2.imwrite(f'imager/{os.path.basename(i)}', resized_img)
-
